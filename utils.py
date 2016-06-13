aa3a6b29428adf8c54849949f56226b2b3a4d6c2
andres <andreslago5@hotmail.com>
2016-06-13 15:12:44 -0300

    Refactoring: Database, model, agent utils now has only the game loop functions
    AgentNN ready waiting for training
    
    Game loop now supports two agents (not eval functions)

--- utils.py
+++ utils.py
@@ -1,24 +1,47 @@
 #
 # X hacia abajo Y hacia la derecha centrado en la esquina sup izq
-from copy import copy
-from copy import deepcopy
-from skimage import data, color, io, img_as_float
-from skimage.color import rgb2hsv,hsv2rgb
-import units
-from map import Tile
-import MySQLdb
 import heapq
-import pygame
-from pygame.locals import *
-import numpy as np
-from skimage import io
-from skimage.transform import resize
-import skimage
-import pylab as plt
 import random
 
-#Convencciones EL TROOPS[0] ES RED Y EL TROOPS[1] ES BLUE
+import MySQLdb
+import numpy as np
+import pylab as plt
+import skimage
+from skimage import io
+from skimage.color import rgb2hsv,hsv2rgb
+from skimage.transform import resize
+
 from batleStub import virtualBattle
+from tools.agent import agresiveAgent
+from tools.database import getRandomGamesDb, saveGameToDb, jointCellTypes
+from tools.model import doTransition, calcReward, checkTerminal
+
+#Convencciones EL TROOPS[0] ES RED Y EL TROOPS[1] ES BLUE
+
+class gameSlider(object):
+    def __init__(self, ax, imageList):
+        self.ax = ax
+        # ax.set_title('use scroll wheel to navigate images')
+
+        self.data = imageList
+        self.ind = 0
+        self.maxImg = len(imageList)
+        self.im = ax.imshow(self.data[self.ind][0])
+        self.update()
+
+    def onscroll(self, event):
+        if event.button == 'up':
+            self.ind = (self.ind - 1) % (self.maxImg)
+        else:
+            self.ind = (self.ind + 1) % (self.maxImg)
+
+        self.update()
+
+    def update(self):
+        self.im.set_data(self.data[self.ind][0])
+        self.ax.set_title(self.data[self.ind][1]+'slice %s' % self.ind)
+        self.im.axes.figure.canvas.draw()
+
 
 testGame = [
                 {
@@ -75,349 +98,8 @@ is_solid
 state_id
 """
 
-def gameToInputNN():
-    pass
-
-def getRandomGamesDb(number,cursor):
-    gameList = []
-    query = "SELECT `id` from `game` ORDER BY RAND() LIMIT %s ;"
-    cursor.execute(query,(number,))
-    result = cursor.fetchall()
-    print result
-    for row in result:
-        ident = int(row['id'])
-        print ident
-        gameList.append(getGamefromDb(ident,cursor))
-    return gameList
-
-def getNStates(number,cursor):
-    states = []
-
-    query = "SELECT `current_state_id` from `state` ORDER BY RAND() LIMIT %s ;"
-    cursor.execute(query,(number,))
-    result = cursor.fetchall()
-    print result
-    for row in result:
-        ident = int(row['current_state_id'])
-
-
-        query = "SELECT * FROM `state`, `cell_states`   WHERE `current_state_id` = %s and `state_id` = `current_state_id` ;"
-        cursor.execute(query,(ident,))
-        cellsState = cursor.fetchall()
-        processed = processCellList(cellsState)
-        #resulting game should have only 1 state
-        assert(len(processed) == 1)
-        #append to states
-        states.append(processed[0])
-
-    return states
-
-#nota 1 ya no es necesario first game id
-def getGamefromDb(idGame,cursor):
-
-    query = "SELECT * FROM `state`, `cell_states`   WHERE `game_id` = %s and `state_id` = `current_state_id` " \
-            "ORDER  BY `order_in_game` "
-    print 'Getting ',idGame
-    cursor.execute(query,(str(idGame),))
-    result = cursor.fetchall()
-
-    return processCellList(result)
-
-
-
-"""
-Given a list of cells process all the states (must follow game_order) return gameList.
-Game list is a list of states.
-"""
-def processCellList(result):
-    listGame = []
-    currentId = -1
-    currentState = None
-
-
-
-    #get all rows in state,cell with game id
-    for cell in result:
-
-        rowId = int(cell['state_id'])
-
-        if rowId != currentId:
-            #change currentRow
-            currentState = {}
-            currentId = rowId
-            #format action
-            currentState['Action'] = {}
-            currentState['Action']['Xi'] = int(cell['xi'])
-            currentState['Action']['Yi'] = int(cell['yi'])
-            currentState['Action']['Xf'] = int(cell['xf'])
-            currentState['Action']['Yf'] = int(cell['yf'])
-            currentState['Action']['Xa'] = int(cell['xa'])
-            currentState['Action']['Ya'] = int(cell['ya'])
-            currentState['Action']['action_type'] = int(cell['action'])
-
-            #format next_terminal
-            currentState['next_terminal'] = int(cell['next_terminal'])
-
-            #initialize terrain, troops as empty lists
-            currentState['Terrain'] = []
-            #Troops contains two list BlueTroops = list[0], RedTroops=list[1]
-            currentState['Troops'] = [[],[]]
-            #append to gameList
-            listGame.append(currentState)
-
-        #format cell things in currentRow object
-        newCellTerrain = {'x':int(cell['x']),'y':int(cell['y']),'Terrain_type':int(cell['terrain_type_id'])}
-
-        currentState['Terrain'].append(newCellTerrain)
-
-        #Only add troop if troop_type != 0
-        troop_type = int(cell['troop_id'])
-        if troop_type != 0:
-            newCellTroop = {'x':int(cell['x']),'y':int(cell['y'])
-                ,'Can_move':int(cell['can_move']),'Troop':int(cell['troop_id']),
-                            'Team':int(cell['team_id']),'HP':int(cell['hp'])
-                            }
-            currentTeam = int(cell['team_id'])
-            currentState['Troops'][currentTeam].append(newCellTroop)
-
-    return listGame
-
-"""
-Take the entire game (list of transitions from start to end ) and save it in the DB
-Game = [ (transition0) , (transition1) ..... ]
-"""
-
-
-def saveGameToDb(cursor, listaTrans, comment):
-
-
-    #insert the first transition returning the transition id
-    firstTrans = listaTrans[0]
-    firstStateId = saveTransitionToDb(cursor,firstTrans,0,0) #Just put any gameId for the moment
-
-    #insert new Game row (first_state, comment)
-
-    insertQuery = "INSERT INTO `game`( `first_state`, `comment`)" \
-                  " VALUES ( %s,%s); "
-
-    cursor.execute(insertQuery,(str(firstStateId),comment))
-    gameId = int(cursor.lastrowid)
-    print 'La game id es ',gameId
-    #edit first transition gameId
-    cursor.execute("UPDATE `state` SET `game_id`= %s"
-                   "  WHERE  `current_state_id`= %s ",(str(gameId),str(firstStateId)))
-
-    #insert all the states
-    for i in range(1,len(listaTrans)):
-        saveTransitionToDb(cursor,listaTrans[i],gameId,i)
-
-    return gameId
-
-"""
-Insert transition into DB and return the STATE_ID given for this transition
-Transition consist of Terrain, Troops and action
-"""
-def saveTransitionToDb(cursor,transition,gameId,orderInGame):
-    query = "INSERT INTO `state`" \
-            "( `game_id`, `xi`, `yi`, `xf`, `yf`, `action`, `order_in_game`," \
-            " `next_terminal`, `xa`, `ya`)" \
-            " VALUES %s ; " \
-            " "
-
-    params = ([gameId,
-              transition['Action']['Xi'],
-              transition['Action']['Yi'],
-              transition['Action']['Xf'],
-              transition['Action']['Yf'],
-              transition['Action']['action_type'],
-              orderInGame,
-              transition['next_terminal'],
-              transition['Action']['Xa'],
-              transition['Action']['Ya']],
-              )
-    # print params
-    #current_state_id	game_id	xi	yi	xf	yf	action	order_in_game	next_terminal
-
-    #insert new state id with action and order return this state_id
-    cursor.execute(query,params)
-    idState = int(cursor.lastrowid)
-    # print 'Id del state ',idState
-
-    #create all the cell given the state_id
-    #id	can_move	x	y	terrain_type_id	troop_id
-    # team_id	hp	state_id
-
-
-
-
-    dictCell = jointCellTypes(transition)
-
-    #Add to string (va1,val2 ... valX), (val1,val2,....), (val1,val2...valX)
-    paramsString = ""
-    for listCell in dictCell.values():
-        listCell.append(idState)
-        paramsString += (str(tuple(listCell))+' , ')
-    paramsString = paramsString[:-2]
-    # print {'Can_move': 0, 'x': 1, 'y': 2, 'Terrain_type': 3, 'Troop': 4, 'Team': 5,
-    #          'HP': 6}
-    # print 'Params to add ',paramsString
-
-
-    #Add all cells to DB
-    query = "INSERT INTO `cell_states` " \
-            "(`can_move`, `x`, `y`, `terrain_type_id`, " \
-            " `troop_id`, `team_id`, `hp`, `state_id`) VALUES %s ; "
-
-    cursor.execute(query % paramsString)
-
-    return idState
-
-def jointCellTypes(transition):
-    dictCell = {}
-    order = {'Can_move': 0, 'x': 1, 'y': 2, 'Terrain_type': 3, 'Troop': 4, 'Team': 5,
-             'HP': 6}
-
-    defaultlist = [0 for i in range(7)]
-
-    # join all cell types
-    for cell in (transition['Troops'][0] + transition['Troops'][1] + transition['Terrain']):
-        if not (dictCell.has_key((cell['x'], cell['y']))):
-            dictCell[(cell['x'], cell['y'])] = copy(defaultlist)
-        for key in cell:
-            dictCell[(cell['x'], cell['y'])][order[key]] = cell[key]
-    return dictCell
-
-
-
-#TODO get next state from currentState, action
-"""
-This function calculate next_state given current_state, action
-THIS FUNCTION CAN MAKE ILEGAL ACTIONS must call a getlegal_actions() to be safe
-
-ACTION_TYPE = 0 Just Move
-ACTION_TYPE = 1 Move and Attack
-
-"""
-def doTransition(state,action):
-
-    #after the action go to wait (troop["Can_move"] = 0)
-    #if you just wait move from Xi to Xf = Xi
-    state = deepcopy(state)
-
-
-    currentTroop = None
-    #get troop in cords
-    for troop in (state["Troops"][0] +state["Troops"][1]) :
-        if troop["x"] == action['Xi'] and troop["y"] == action['Yi']:
-            currentTroop = troop
-
-    current_team = currentTroop["Team"]
-
-    #Could it move?
-    assert(currentTroop["Can_move"])
-    #move to Xf,Yf
-    currentTroop['x'] = action['Xf']
-    currentTroop['y'] = action['Yf']
-
-    #Action_type = 0 just move so we end TROOP_TURN
-    if action["action_type"] == 0:
-        currentTroop["Can_move"] = 0
-
-
-    #Action_type = 1 Attack_Move. Troop A in (Xi,Yi) attacks Troop B in (Xa,Ya)
-    if action["action_type"] == 1:
-
-        #get troop A
-        troopA = currentTroop
-        #getTroopB
-        troopB = None
-
-
-        troopB = [troop for troop in state["Troops"][1-current_team] if ((troop["x"] == action['Xa']) and (troop["y"] == action['Ya']))][0]
-
-
-
-        #get env
-
-        terrAly = [terrain for terrain in state["Terrain"] if ((terrain["x"] == action['Xi']) and (terrain["y"] == action['Yi']))][0]
-        terrEnemy = [terrain for terrain in state["Terrain"] if ((terrain["x"] == action['Xa']) and (terrain["y"] == action['Ya']))][0]
-
-        atkEnv = Tile.defenseValues[terrAly['Terrain_type']]
-        defEnv = Tile.defenseValues[terrEnemy['Terrain_type']]
-
-        #Crear tropa simulada desde tropa jason
-        attacker =  units.troopFromJson(troopA)
-        defender = units.troopFromJson(troopB)
-
-        #realiza calculo de ataque
-
-
-        #End troop TURN so we wait (just the attacker wait)
-        troopA["Can_move"] = 0
-
-        #calculo damage defender
-        defender.health -= attacker.getAttackDamage(defender, defEnv)
-        #Si hp defensor es 0 eliminar de tropas
-        if defender.health <= 0:
-            state["Troops"][1-current_team].remove(troopB)
-        else:
-            troopB["HP"] = defender.health
-            if not attacker.isArtilleryUnit and not defender.isArtilleryUnit:
-                attacker.health -= defender.getRetaliatoryDamage(attacker, atkEnv)
-                #si hp de atacante es cero eliminar de troops
-                if attacker.health <= 0:
-                    state["Troops"][current_team].remove(troopA)
-                else:
-                    troopA["HP"] = attacker.health
-
-        #Hp actualizado
-
-
-
-
-    return state
-
-"""
-Reward for state,action,nextState
-For the first try the Network will estimate Q(s,a) as the EXPECTED COST from S taking action A.
-The agent try to take the minium cost path (path to goal WIN STATE).
-
-No enemys left = 0 (No cost Best case scenario)
-No troops left = 1 (Maxium cost Worst case)
-Else living penalty = 0.2 as an example (this make the agent end as quicly as posible)
-"""
-def calcReward(state,action,nextState,currentTurn):
-
-    #teams are 0 or 1
-    aly = currentTurn
-    enemy = 1-aly
-
-    lostGame = 1
-    win = 0
-    livingPenalty = 0.2
-
-    if len(nextState["Troops"][aly]) == 0:
-        return lostGame
-    if len(nextState["Troops"][enemy]) == 0:
-        return win
-    else:
-        return livingPenalty
-
-def checkTerminal(state,currentTurn):
-    if len(state["Troops"][1-currentTurn]) == 0:
-        print "Game ended in WIN for ",currentTurn
-        return 1
-    if len(state["Troops"][currentTurn]) == 0:
-        print "Game ended in LOSE for ", currentTurn
-        return -1
-    else:
-        return 0
-
-#TODO encodeAction to Json
-def encodeActionJson():
-    pass
-
-def gameLoop(baseBattle,initialState,funStateAction,store=True,randomProb=0.5,cursor=None):
+#NOW RANDOM PROB IS inside agent behavior DEFAULT IS 0
+def gameLoop(baseBattle, initialState, agentRed,agentBlue, store=True,cursor=None):
 
     #just to avoid infinite loops
     maxIter = 100
@@ -433,18 +115,17 @@ def gameLoop(baseBattle,initialState,funStateAction,store=True,randomProb=0.5,cu
     #Is red or blue turn?
     activeTeam = 0
 
-    while checkTerminal(state,activeTeam) == 0:
-
+    while checkTerminal(state, activeTeam) == 0:
+        agent = agentRed if activeTeam == 0 else agentBlue
         for troop in state['Troops'][activeTeam]:
             heapAction = [] #The python heap keep TUPLES (value,action) sorted by value like a heap
             accionesValidas = baseBattle.getPossibleMoves((troop['x'], troop['y']))
 
-
             #Random action or evaluation ?
             randomNumber = random.random()
-            if randomNumber < randomProb:
+            if randomNumber < agent.randomProb:
                 for action in accionesValidas:
-                    value = funStateAction(state,action)
+                    value = agent.evalAction(state, action,activeTeam)
                     heapq.heappush(heapAction,(value,action))
                 bestValue, bestAction = heapq.heappop(heapAction)
                 # print "Best action to choose ",bestAction
@@ -452,11 +133,11 @@ def gameLoop(baseBattle,initialState,funStateAction,store=True,randomProb=0.5,cu
                 bestAction = random.choice(accionesValidas)
                 bestValue = -1
 
-            nextState = doTransition(state,bestAction)
-            reward = calcReward(state, bestAction, nextState,activeTeam)
+            nextState = doTransition(state, bestAction)
+            reward = calcReward(state, bestAction, nextState, activeTeam)
 
             #place if next state is win,lose or non terminal
-            state['next_terminal'] = checkTerminal(nextState,activeTeam)
+            state['next_terminal'] = checkTerminal(nextState, activeTeam)
             #set Action to state
             state['Action'] = bestAction
 
@@ -487,13 +168,13 @@ def gameLoop(baseBattle,initialState,funStateAction,store=True,randomProb=0.5,cu
     #place terminal in last transition
     if store:
         #open db and store game
-        saveGameToDb(cursor,game,'GameComment')
+        saveGameToDb(cursor, game, 'GameComment')
 
     return game
 
 
 
-def generateTestCase(cursor,store=True):
+def generateGame(cursor, store=True):
 
     #TODO create map independent from battle. A lot of innecesary stuff there and pygame is heavy
 
@@ -505,15 +186,16 @@ def generateTestCase(cursor,store=True):
 
     #Send agresive evaluation. Always try to attack
     #If action type == 1 is an attack then eval to 1 else eval to 0
-    evalfun = lambda state,action : -1*(action['action_type'])
+    agresiveRed = agresiveAgent()
+    agresiveBlue = agresiveAgent()
 
-    game = gameLoop(batalla,initialState,evalfun,store=store,cursor=cursor)
+    game = gameLoop(batalla,initialState,agresiveRed,agresiveBlue,store=store,cursor=cursor)
 
     print "Game generation ended ",len(game)," transitions"
     return game
 
 
-def showTransition(transition):
+def showTransition(transition,dontShow=False):
     rutaTiles = "tiles/"
     rutaTroops="units/"
     dictMap = {0:'WaterOpen.png',
@@ -609,45 +291,52 @@ def showTransition(transition):
         y = transition['Action']["Xi"] * tileDim
         base[y:y + tileDim, x:x + tileDim,:3] = temp[y:y + tileDim, x:x + tileDim]
 
-        plt.suptitle(
-            "Xi, Yi : " + str((transition['Action']["Xi"], transition['Action']["Yi"])) + " " +
-            "Xf, Yf : " + str((transition['Action']["Xf"], transition['Action']["Yf"])) + " " +
-            "Type : " + str(transition['Action']["action_type"]) + " "
-            "Xa, Ya : " + str((transition['Action']["Xa"], transition['Action']["Ya"])) + " ")
+        stringOut = "Xi, Yi : " + str((transition['Action']["Xi"],
+                                       transition['Action']["Yi"])) + " " +"Xf, Yf : " + \
+                    str((transition['Action']["Xf"], transition['Action']["Yf"])) + " " +"Type : " +\
+                    str(transition['Action']["action_type"]) + " ""Xa, Ya : " + str((transition['Action']["Xa"],
+                                                                                     transition['Action']["Ya"])) + " "
+
+    if dontShow:
+        return (base,stringOut)
+    plt.suptitle(stringOut)
     plt.imshow(base)
     plt.show()
 
+def showGameScroll(gameList):
+
+
+    imageList = [showTransition(state, True) for state in gameList]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    tracker = gameSlider(ax, imageList)
+
+    fig.canvas.mpl_connect('scroll_event', tracker.onscroll)
+    plt.show()
 
 
 if __name__ == '__main__':
-    # db = MySQLdb.connect("200.9.100.170", "bayes", "yesbayesyes", "bayes")
-    # # cursor = db.cursor()
-    # cursor = db.cursor (MySQLdb.cursors.DictCursor)
-    # saveGameToDb(cursor,testGame,'Este es el primer das code')
-    # # getGamefromDb(16,cursor)
-    #
-    # cursor.close()
-    # db.commit()
-    # db.close()
 
-    db = MySQLdb.connect("200.9.100.170", "bayes", "yesbayesyes", "bayes")
+
+    db = db = MySQLdb.connect("200.9.100.170", "bayes", "yesbayesyes", "bayes")
     cursor = db.cursor()
     cursor = db.cursor (MySQLdb.cursors.DictCursor)
 
-    getNStates(3,cursor)
-    #
+
     # mode = "showRandomGames"
-    # # mode = "generateRandomGames"
-    #
-    # if mode == "generateRandomGames":
-    #     for i in range(300):
-    #         game = generateTestCase(cursor,True)
-    #
-    # if mode == "showRandomGames":
-    #     gameList = getRandomGamesDb(2,cursor)
-    #     for game in gameList:
-    #         for transition in game:
-    #             showTransition(transition)
+    mode = "generateRandomGames"
+
+    if mode == "generateRandomGames":
+        for i in range(2):
+            game = generateGame(cursor, False)
+
+    if mode == "showRandomGames":
+        gameList = getRandomGamesDb(2, cursor)
+        #If goes too slow use for state in game : showTransition(state) JUST FOR BETTER VISUALIZATION
+        for game in gameList:
+            showGameScroll(game)
 
     cursor.close()
     db.commit()
