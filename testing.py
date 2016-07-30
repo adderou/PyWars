833484d8cf6a14bc1e60541f51b188eb77bc5c84
adderou <dudu@adderou.cl>
2016-07-29 23:49:33 -0400

    Revision detalle movimiientos posibles

--- testing.py
+++ testing.py
@@ -5,6 +5,8 @@ import utils as utils
 import main as main
 import tools.agent as agent
 import MySQLdb
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -14,22 +16,33 @@ if __name__ == "__main__":
     cursor = db.cursor (MySQLdb.cursors.DictCursor)
 
     print "Generando gráficos de éxito de Red Neuronal vs. Agresivo Aleatorio"
-    hidenUnitsList = [50,100,1000]
-    trainingSamplesList = [500,5000,30000]
+    hidenUnitsList = [5,10,20,50,100,500]
+    fixedUnit = 100
+    trainingSamplesList = [10,50,100,500,5000,30000]
+    fixedSample = 30000
     whoStartList = [-1,0,1]
     whoStartType = {-1:"Aleatorio",0:"Parte Red Neuronal",1:"Parte Agresivo Aleatorio"}
-    iteraciones = 10
-    valuesArray = []
+    iteraciones = 20
     blueAgent = agent.agresiveAgent()
     for whoStart in whoStartList:
-        print "Probando con configuración"
-        valuesArray.append([])
+        valuesArray = []
         for hidenUnits in hidenUnitsList:
-            print "Usando hidenUnits =",hidenUnits
-            valuesArray[len(valuesArray)-1].append([])
-            for trainingSamples in trainingSamplesList:
-                print "Usando trainingSamples =",trainingSamples
-                redAgent = main.createTrainedNeuralAgent(hidenUnits,trainingSamples)
-                values = utils.testAgentRed(cursor,redAgent,blueAgent,iteraciones)
-                valuesArray[len(valuesArray)-1][len(valuesArray[0])].append(values)
-    print valuesArray
+            print "Probando con configuración: ", whoStartType[whoStart],"HidenUnits:",hidenUnits,"trainingSamples",fixedSample
+            redAgent = main.createTrainedNeuralAgent(hidenUnits, fixedSample)
+            values = utils.testAgentRed(cursor, redAgent, blueAgent, iteraciones)
+            valuesArray.append(values["won"])
+        plt.plot(hidenUnitsList,valuesArray)
+        plt.title("Victorias Red Neuronal "+whoStartType[whoStart]+" y trainingSamples = ",fixedSample)
+        plt.ylabel('% Ganado')
+        plt.xlabel('Valor hiddenUnits')
+        plt.show()
+        for trainingSamples in trainingSamplesList:
+            print "Probando con configuración: ", whoStartType[whoStart],"HidenUnits:",hidenUnits,"trainingSamples",fixedSample
+            redAgent = main.createTrainedNeuralAgent(fixedUnit, trainingSamples)
+            values = utils.testAgentRed(cursor, redAgent, blueAgent, iteraciones)
+            valuesArray.append(values["won"])
+        plt.plot(trainingSamplesList,valuesArray)
+        plt.title("Victorias Red Neuronal "+whoStartType[whoStart]+" y hiddenUnits = ",fixedUnit)
+        plt.ylabel('% Ganado')
+        plt.xlabel('Valor hiddenUnits')
+        plt.show()
